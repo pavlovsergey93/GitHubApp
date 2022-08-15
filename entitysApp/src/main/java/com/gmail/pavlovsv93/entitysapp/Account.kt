ad47440a528b725db1b6725280078c2643dfda85
Pavlov Sergey <pavlov.androdev@gmail.com>
2022-08-15 16:10:11 +0300

    Entities App

---
+++ entitysApp/src/main/java/com/gmail/pavlovsv93/entitysapp/Account.kt
@@ -0,0 +1,9 @@
+package com.gmail.pavlovsv93.entitysapp
+
+data class Account(
+	val id: Int,
+	val avatarUrl: String?,
+	val htmlUrl: String?,
+	val login: String,
+	val reposUrl: String?
+)
---
+++ entitysApp/src/main/java/com/gmail/pavlovsv93/entitysapp/RepositoriesAccount.kt
@@ -0,0 +1,10 @@
+package com.gmail.pavlovsv93.entitysapp
+
+data class RepositoriesAccount(
+	val id: Int,
+	val name: String,
+	val nodeId: String,
+	val description: String?,
+	val url: String,
+	val language: String?
+)
